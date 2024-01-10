0477f01e789beeeef727c2f2398adaf67770b55f
Saibhavithra <saibhavithra@gmail.com>
2024-01-10 08:33:20 -0600

    Login Validation

--- web-demo/src/main/java/com/training/web/HomeServlet.java
+++ web-demo/src/main/java/com/training/web/HomeServlet.java
@@ -1,17 +1,27 @@
 package com.training.web;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+
+//import com.training.web.dao.LoginDao;
+import com.training.web.dao.UserDAO;
+import com.training.web.model.User;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class HomeServlet
  */
 @WebServlet("/home")
+
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -22,17 +32,29 @@ public class HomeServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+String msg;
+String errmsg;
+    public void init() {
+    	msg ="Sitaram";
+    	errmsg = "error";
+    }
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.setContentType("text/html");
+		/*response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.print("<html><body>");
-		out.print("<h1>Hello</h1>");
-		out.print("</body></html>");
+	//	out.print("<h1>Hello</h1>");
+		out.print("<h1>Hello "+msg+"from servlet!</h1>");
+		out.print("</body></html>");*/
+	/*	request.setAttribute("name",msg);
+		RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+		rd.forward(request, response);
+		//response.sendRedirect("home.jsp");*/
+		
 		
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -42,7 +64,65 @@ public class HomeServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		//doGet(request, response);
+		String name= request.getParameter("name");
+		String password= request.getParameter("password");
+		
+		UserDAO userDao = new UserDAO();
+		
+		  try {
+			  User user = userDao.checkLogin(name, password);
+	           // String destPage = "home.jsp" ;
+	             
+	            if (user != null) {
+	               // HttpSession session = request.getSession();
+	                request.setAttribute("user", user);
+	               // destPage = "home.jsp";
+	                
+	                RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+		            dispatcher.forward(request, response);
+	            	
+	            /*	request.setAttribute("name",name);		
+	        		RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+	        		rd.forward(request, response);*/
+	        		
+	            }
+	            
+	            	
+	            	else 
+	            		
+	            	{
+	                String message = "Invalid username/password";
+	                request.setAttribute("message", message);
+	                
+	                RequestDispatcher dispatcher = request.getRequestDispatcher("test.jsp");
+		            dispatcher.forward(request, response);
+	            	
+	            }
+	            		
+	            	/*{request.setAttribute("output",errmsg);
+	    			RequestDispatcher rd = request.getRequestDispatcher("test.jsp");
+	    			rd.forward(request, response);}*/
+	            
+		/*if ( username.equals("sai")  && password .equals( "1234"))
+		{request.setAttribute("name",username);		
+		RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+		rd.forward(request, response);}
+		else 
+			{request.setAttribute("output",errmsg);
+			RequestDispatcher rd = request.getRequestDispatcher("test.jsp");
+			rd.forward(request, response);}
+		
+		
+	}*/
+	            
+	            
+	         /*   RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+	            dispatcher.forward(request, response);*/
+	             
+	        } catch (SQLException | ClassNotFoundException ex) {
+	            throw new ServletException(ex);
+	        }
 
+}
 }
---
+++ web-demo/src/main/java/com/training/web/model/User.java
@@ -0,0 +1,35 @@
+package com.training.web.model;
+/*import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString*/
+
+
+
+public class User {
+	
+	private String name;
+    private String password;
+ 
+   
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+    // getters and setters are not shown for brevity   
+     
+}
